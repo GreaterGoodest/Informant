c32efdfe90aa4eefd7085d8db404597b6ac99b79
Ryan Good <usafaryangood@gmail.com>
2021-03-03 19:29:21 -0600

    adds shellcode extraction stage

--- payloads/hello_world.asm
+++ payloads/hello_world.asm
@@ -1,15 +1,11 @@
-SECTION .DATA
-    helloString:        db 'Hello world!',10 ; 10 = \n
-    helloStringLength:  equ $-helloString    ; $ = current location.
-
 SECTION .TEXT
     GLOBAL _start
 
 _start:
     mov rax, 4                  ; 4 = write
     mov rbx, 1                  ; file descriptor = stdout
-    mov rcx, helloString 
-    mov rdx, helloStringLength
+    mov rcx, 'hello'
+    mov rdx, 5
     int 80h                     ; syscall
 
     mov rax, 1                  ; exit for now (TODO: recover)
